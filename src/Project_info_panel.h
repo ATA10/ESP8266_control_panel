a5d11b782ce1821279697e99cde8ab9ebcca1757
aniltaha <60629661+ATA10@users.noreply.github.com>
2025-03-05 00:43:03 +0300

    1

--- src/Project_info_panel.h
+++ src/Project_info_panel.h
@@ -3,12 +3,6 @@
 
 #include <Arduino.h>
 
-
-const String deviceID = "NuCntIHjaq8EnAsOShff";
-
-const String projectId = "iotroshark";  // Firebase Proje ID
-const String apiKey = "AIzaSyC6woJzfVXfyYxu8FVi8HwDWlB6VAgaxNo";  // Firebase API Key
-
 // const String deviceID = "YOUR_FIRESTORE_DOCUMENT";
 
 // const String projectId = "YOUR_FIRESTORE_NAME";  // Firebase Proje ID
@@ -16,27 +10,3 @@ const String apiKey = "AIzaSyC6woJzfVXfyYxu8FVi8HwDWlB6VAgaxNo";  // Firebase AP
 
 #endif
 
-
-
-// "fields": {
-//     "relays": {
-//       "mapValue": {
-//         "fields": {
-//           "relay1": {
-//             "booleanValue": false
-//           },
-//           "relay2": {
-//             "booleanValue": true
-//           }
-//         }
-//       }
-//     },
-//     "sensors" : {
-//        "mapValue": {
-//           "fields": {
-//            "humidity":  "722",
-//             "temperature": "9.639"         
-//           }
-//         } 
-//     }
-//   },
